8806990e1285679457589edb04d071f74ecea34e
Aakash Jha <aakash.jha@daimler.com>
2020-09-28 18:54:12 +0530

    Updates

---
+++ precache-manifest.a9fa2e71083a0523bdd9844c1765509b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ef50280161ea21e32e03bf382051ff",
+    "url": "/got-to-do/index.html"
+  },
+  {
+    "revision": "bf4c1357006df752f51b",
+    "url": "/got-to-do/static/css/main.c8061b19.chunk.css"
+  },
+  {
+    "revision": "1195f4b69fa408702aab",
+    "url": "/got-to-do/static/js/2.ef636463.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/got-to-do/static/js/2.ef636463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4c1357006df752f51b",
+    "url": "/got-to-do/static/js/main.a2cf468f.chunk.js"
+  },
+  {
+    "revision": "4faf55bbc915494c7def",
+    "url": "/got-to-do/static/js/runtime-main.25b71204.js"
+  }
+]);
